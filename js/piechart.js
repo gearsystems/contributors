411b4bfd847e63b16ec48a49931ac1432d2d93e6
arjun0507 <arjun.bhargava.0507@gmail.com>
2015-03-30 23:42:59 +0530

    Fixes #14 Adds bar chart functionality

--- js/piechart.js
+++ js/piechart.js
@@ -96,6 +96,84 @@ $(document).ready(function(){
             };
             var chart = new google.visualization.PieChart(document.getElementById('Deletions'));
             chart.draw(data, options);
+
+
+
+
+            //Commits
+            var data = google.visualization.arrayToDataTable([
+                 [final[0], final[1], { role: 'style' }],
+                 [final[4], final[5], 'red']
+              ]);
+            for (var i = 2; i < (final.length)/4; i++ ){
+                data.addRows([[final[(4*i)],final[(4*i)+1], 'red']]);
+            }
+
+              var view = new google.visualization.DataView(data);
+              view.setColumns([0, 1,
+                               { calc: "stringify",
+                                 sourceColumn: 1,
+                                 type: "string",
+                                 role: "annotation" },
+                               2]);
+            var options = {
+                title: "Commits",
+                bar: {groupWidth: "95%"},
+                legend: { position: "none" },
+            };
+            var chart = new google.visualization.BarChart(document.getElementById('barchart_commit'));
+            chart.draw(view, options);
+
+
+            //Additions
+            var data = google.visualization.arrayToDataTable([
+                 [final[0], final[1], { role: 'style' }],
+                 [final[4], final[6], 'green']
+              ]);
+            for (var i = 2; i < (final.length)/4; i++ ){
+                data.addRows([[final[(4*i)],final[(4*i)+2], 'green']]);
+            }
+
+              var view = new google.visualization.DataView(data);
+              view.setColumns([0, 1,
+                               { calc: "stringify",
+                                 sourceColumn: 1,
+                                 type: "string",
+                                 role: "annotation" },
+                               2]);
+            var options = {
+                title: "Additions",
+                bar: {groupWidth: "95%"},
+                legend: { position: "none" },
+            };
+            var chart = new google.visualization.BarChart(document.getElementById('barchart_addition'));
+            chart.draw(view, options);
+
+
+            
+            //Deletions
+            var data = google.visualization.arrayToDataTable([
+                 [final[0], final[1], { role: 'style' }],
+                 [final[4], final[7], 'blueviolet']
+              ]);
+            for (var i = 2; i < (final.length)/4; i++ ){
+                data.addRows([[final[(4*i)],final[(4*i)+3], 'blueviolet']]);
+            }
+
+              var view = new google.visualization.DataView(data);
+              view.setColumns([0, 1,
+                               { calc: "stringify",
+                                 sourceColumn: 1,
+                                 type: "string",
+                                 role: "annotation" },
+                               2]);
+            var options = {
+                title: "Deletions",
+                bar: {groupWidth: "95%"},
+                legend: { position: "none" },
+            };
+            var chart = new google.visualization.BarChart(document.getElementById('barchart_deletion'));
+            chart.draw(view, options);
         }
     });
 });
